e4e1fc8e2e9c9f91d8c2cab1d8cd680d46eea9c9
Matheus G. Dias <matheusgiga123@gmail.com>
2025-04-16 22:33:56 -0300

    add clang format

--- src/main.c
+++ src/main.c
@@ -1,16 +1,13 @@
-#include "raylib.h"
-#include "stdio.h"
-
-#include "config.h"
 #include "assets.h"
+#include "config.h"
+#include "raylib.h"
 #include "screens/main_menu.h"
 
 void handleScreens(int screen);
 void renderScreens(int screen);
 void drawMouseImage(Texture2D mouse);
 
-int main(void)
-{
+int main(void) {
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Platform Game");
 
     int currentScreen = SCREEN_MAIN_MENU;
@@ -18,8 +15,7 @@ int main(void)
     LoadTexturesAssets();
     InitMainMenu();
 
-    while (!WindowShouldClose())
-    {
+    while (!WindowShouldClose()) {
         handleScreens(currentScreen);
 
         BeginDrawing();
@@ -39,28 +35,23 @@ int main(void)
     return 0;
 }
 
-void drawMouseImage(Texture2D mouse)
-{
+void drawMouseImage(Texture2D mouse) {
     HideCursor();
     DrawTextureV(mouse, GetMousePosition(), WHITE);
 }
 
-void handleScreens(int screen)
-{
-    if (screen == SCREEN_MAIN_MENU && IsKeyDown(KEY_ENTER))
-    {
+void handleScreens(int screen) {
+    if (screen == SCREEN_MAIN_MENU && IsKeyDown(KEY_ENTER)) {
         UnloadMainMenu();
     }
 }
 
-void renderScreens(int screen)
-{
-    switch (screen)
-    {
-    case SCREEN_MAIN_MENU:
-        DrawMainMenu();
-        break;
-    default:
-        break;
+void renderScreens(int screen) {
+    switch (screen) {
+        case SCREEN_MAIN_MENU:
+            DrawMainMenu();
+            break;
+        default:
+            break;
     }
 }
